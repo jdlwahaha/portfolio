3e1b6f8c4cfefd9c95a457643b53f07e479f47fa
JDL WAHAHA <>
2020-01-05 08:09:01 -0800

    Fix link

--- js/script.js
+++ js/script.js
@@ -5,33 +5,34 @@
     }); 
     
     
+
     function loadHeader() { 
-        var headerHtml = `
-            <h1>My Little Space</h1>
-            <nav>
-                <!-- <a href="index.html" class="first">Home</a> -->
-                <a href="jdreads.html" class="first">JDReads</a>
-                <a href="quotes.html">Quotes</a>
-                <!-- <a href="books.html">Books</a> -->
-            </nav>
-        `;
+        var headerHtml = 
+            '<h1>My Little Space</h1>' +
+            '<nav>' +
+                '<!-- <a href="index.html" class="first">Home</a> -->' +
+                '<a href="jdreads.html" class="first">JDReads</a>' +
+                '<a href="quotes.html">Quotes</a>' +
+                '<!-- <a href="books.html">Books</a> -->' +
+            '</nav>';
         $('header').append(headerHtml); 
+        
     
         // read url and highlight tab 
-        var pages = ['index', 'quotes', 'books', 'jdreads']; 
+        var pages = [//'index',
+             'quotes', 'books', 'jdreads']; 
     
         let index = window.location.href.indexOf('portfolio/') + 'portfolio/'.length; 
         let currentPageStr = window.location.href.substring(index); 
     
-        let currentPage = pages.find(x => currentPageStr.includes(x));
+        let currentPage = pages.find(x => currentPageStr.indexOf(x)>=0);
         if (currentPage) { 
             $(`a[href="${currentPage}.html"]`).addClass('current-tab');
         } else { 
             // redirect to home page
-            // window.location.href = '/';
+            window.location.href = window.location.origin + '/portfolio/jdreads.html'; 
         }
     
-        
     }
     
     
