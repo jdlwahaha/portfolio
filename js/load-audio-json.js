a81569d89707ae1a9041521272ceada869b79ac4
JDL Wahaha <jdlwahaha@JDLs-Air.hitronhub.home>
2021-03-06 18:52:59 -0800

    Site redesign

--- js/load-audio-json.js
+++ js/load-audio-json.js
@@ -11,6 +11,9 @@ $(document).ready(function() {
 
         // $('#total2020').append(recordings.filter(r => r.recording_date.substr(0, 4) === '2020').length);
         $('#total2021').append(recordings.filter(r => r.recording_date.substr(0, 4) === '2021').length);
+        $('#total2020').append(recordings.filter(r => r.recording_date.substr(0, 4) === '2020').length);
+        $('#total2019').append(recordings.filter(r => r.recording_date.substr(0, 4) === '2019').length);
+        $('#total2018').append(recordings.filter(r => r.recording_date.substr(0, 4) === '2018').length);
 
         // paging
         let pageNum = parseInt(getQueryParam('page'));
--- js/script.js
+++ js/script.js
@@ -8,11 +8,12 @@
 
     function loadHeader() { 
         var headerHtml = `
-            <h1>My Little Space</h1>
+            <h1>JDReads</h1>
             <nav>
                 <!-- <a href="index.html" class="first">Home</a> -->
                 <a href="jdreads.html" class="first">JDReads</a>
-                <a href="quotes.html">Quotes</a>
+                <!-- <a href="quotes.html">Quotes</a> -->
+                <a href="about.html">About</a>
                 <!-- <a href="books.html">Books</a> -->
             </nav>
         `;
@@ -20,12 +21,12 @@
         
     
         // read url and highlight tab 
-        var pages = ['index','quotes', 'books', 'jdreads']; 
+        var pages = ['index', 'jdreads', 'about']; 
     
         let index = window.location.href.indexOf('portfolio/') + 'portfolio/'.length; 
         let currentPageStr = window.location.href.substring(index); 
     
-        let currentPage = pages.find(x => currentPageStr.includes(x));
+        let currentPage = pages.find(x => currentPageStr.includes(x.toLowerCase()));
         if (currentPage) { 
             $(`a[href="${currentPage}.html"]`).addClass('current-tab');
         } else { 
