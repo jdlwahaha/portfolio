63c98692c1f4058850a48163ac416c42c460c41d
JDL Wahaha <jdlwahaha@JDLs-MacBook-Air.local>
2022-11-01 18:44:55 -0700

    fix stlying and add selection for youtube list by month view

--- js/youtube-list.js
+++ js/youtube-list.js
@@ -1,31 +1,88 @@
 (function () {
-    $(document).ready(function () {
         
-        $(document).ready(function () {
-            $.getJSON('data/youtube2.json', function (videos) {
-                const totalPublishThisYear = videos.length - 31; //31 are videos done before 2021
-                loadProgressBar('youtube', totalPublishThisYear, 50);
+    $(document).ready(function () {
+        $.getJSON('data/youtube2.json', function (videos) {
+            const YEAR_START = 2020; 
+            const YEAR_END = 2022; 
 
-                const validVideos = videos.filter(v => v.videoId);
-                let htmlContent = loadVideoLinks(validVideos);
-                $('#youtube_embed_videos').append(htmlContent);
+            loadYearData(2022, videos);
+            var that = this; 
+            that.videos = videos;
+            
+            $(document).on('change', '#yearSelection', function(value){
+                $(`section #a`).empty();
+                const selectedYear = $(this).find("option:selected").attr('value');
+                loadYearData(selectedYear, that.videos);
+
             });
 
-            
+
         });
 
+        
     });
 
 
-    function loadVideoLinks(videos) {
+    function loadYearData(year, videos) { 
+        if (typeof year === 'string') { 
+            year = parseInt(year);
+        }
+
+        const GOAL_FOR_EACH_MONTH = 10; 
+
+        for (let month=11; month >= 0; month--) { 
+            const videosThisMonth = videos.filter(v => v.date.year === year && v.date.month === month);
+            console.log(videosThisMonth);
+            if (videosThisMonth.length > 0) { 
+                const htmlContent = getHtml(year, month, videosThisMonth, GOAL_FOR_EACH_MONTH);
+                $(`section #a`).append(htmlContent);
+            }
+ 
+        }
+
+    }
+
+
+    function getHtml(year, month, videos, video_num_goal) { 
+        const current_count = videos.length;
+        let htmlContent = generalVideoLinks(videos);
+        let percent = Math.floor(1.0 * current_count / video_num_goal * 100);
+        const html = `
+            <div class="date-info">${getMonthName(month)} </div>
+            <div class="meter">
+                <span class="progressbar" style="width: ${percent}%"></span>
+                <strong>
+                    <span class="progress">${current_count}</span> out of 
+                    <span class="goal">${video_num_goal}</span>
+                </strong>
+            </div>
+            <br>
+            <div class="youtube_embed_videos">
+                ${htmlContent}
+            </div>
+            <br>
+            <br>
+        `; 
+        return html; 
+    }
+
+    function generalVideoLinks(videos) {
         let htmlContent = '';
         videos.map(video => {
+            const videoDate = video.date.year + '-' + appendZero(video.date.month+1) + '-' + appendZero(video.date.day);
             if (video.videoId) {
                 const link = `https://youtu.be/${video.videoId}`
-                htmlContent += `<a href="${link}" target="_blank">${video.title}</a><br>`
+                htmlContent += `${videoDate} <a href="${link}" target="_blank">${video.title}</a><br>`
             }
         });
         return htmlContent;
+    }
+
+    function appendZero(number) { 
+        if (number < 10) { 
+            return '0' + number; 
+        }
+        return number; 
     }
 
     function loadIframe(videos) {
@@ -39,15 +96,12 @@
     }
 
 
-    function loadProgressBar(elementId, currentCount, goalCount) {
-        let percent = Math.floor(1.0 * currentCount / goalCount * 100);
-        console.log(percent);
-        $(`#${elementId} .progress`).append(currentCount);
-        $(`#${elementId} .goal`).append(goalCount);
-        $(`#${elementId} span.progressbar`).attr('style', `width: ${percent}%`);
+
+    function getMonthName(monthNumber) {
+        const date = new Date();
+        date.setMonth(monthNumber);
+        return date.toLocaleString('en-US', { month: 'long' });
     }
-
-
   
 
 
