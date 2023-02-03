aeb566eb7529428de38a57170c5827eb87a704ec
JDL Wahaha <z29321136@hotmail.com>
2023-02-03 12:05:59 -0800

    update schema and add exercise page

--- js/books/books.service.js
+++ js/books/books.service.js
@@ -10,22 +10,22 @@ function loadBooks(books) {
 
 
     books.map(function (book) {
-        const bookThumbnail = `../../data/books/thumbnails/` + book.thumbnail;
+        const bookThumbnail = `../../data/books/thumbnails/${book.filename}.png`;
+
+        
 
 
         content += `
             <section class="box">
                 <div class="box-content">
                     <span class="thumbnail-container">
-                        <img src="${bookThumbnail}" alt="${book.thumbnail}">
+                        <img src="${bookThumbnail}" alt="${book.filename}.png">
                     </span>
                     <div class="book-container">
                         <h4>${book.title}</h4>
                         <div class="author">by ${book.author}</div>
                         <div class="rating"> ${getRatingString(book.rating)}</div>
-                        <p class="review">
-                            ${book.review}
-                        </p>
+                        ${getReivew(book)}
                     </div>
                 </div>
             </section>
@@ -36,6 +36,24 @@ function loadBooks(books) {
 
     $('#book_list').append(content);
 
+}
+
+function getReivew(book) { 
+    if (book.showFullReview === true) {
+        return `
+            <p>
+                ${book.review} 
+            </p><p>
+                <a href="./z_exercise-${book.filename}">See More >>></a>
+            </p>
+        `
+    } else { 
+        return `
+            <p class="review">
+                ${book.review}
+            </p>
+        `
+    }
 }
 
 function getRatingString(number) { 
