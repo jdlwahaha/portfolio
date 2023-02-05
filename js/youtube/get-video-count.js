e9dc6ba728e5ddd6ad5bb6a3d72db0a94701f7e6
JDL Wahaha <z29321136@hotmail.com>
2023-02-05 04:17:28 -0800

    fix count list

--- js/youtube/get-video-count.js
+++ js/youtube/get-video-count.js
@@ -2,22 +2,26 @@
 
     
     $(document).ready(function() { 
-        $.getJSON('../../data/videos/go.json', function(videos) {         
-    
-            // remove empty records
-            videos = videos.filter((r) => {return r.title != ''})
-    
-            $('#total2023').append(getVideosCount(videos, 2023));
-            $('#total2022').append(getVideosCount(videos, 2022));
-            $('#total2021').append(getVideosCount(videos, 2021));
-            $('#total2020').append(getVideosCount(videos, 2020));
+        $.getJSON('../../data/videos/ya_deck.json', function(deckVideos) { 
+            $.getJSON('../../data/videos/go.json', function(goVideos) {         
+                let videos = [...goVideos, ...deckVideos];
 
-            // shorts
-            $('#total2023short').append(getShortsCount(videos, 2023));
-            $('#total2022short').append(getShortsCount(videos, 2022));
+                // remove empty records
+                videos = videos.filter((r) => {return r.title != ''})
+        
+                $('#total2023').append(getVideosCount(videos, 2023));
+                $('#total2022').append(getVideosCount(videos, 2022));
+                $('#total2021').append(getVideosCount(videos, 2021));
+                $('#total2020').append(getVideosCount(videos, 2020));
+
+                // shorts
+                $('#total2023short').append(getShortsCount(videos, 2023));
+                $('#total2022short').append(getShortsCount(videos, 2022));
 
 
+            });
         });
+
 
         function getVideosCount(videos, year) { 
             return videos.filter(video => (video.date.year === year && video.type !== 'short')).length;
@@ -26,6 +30,7 @@
         function getShortsCount(videos, year) { 
             return videos.filter(video => (video.date.year === year && video.type === 'short')).length;
         }
+
     });
     
  
