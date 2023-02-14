d668c1a55ffe19517fc5b6883bcba9b18ff48422
JDL Wahaha <z29321136@hotmail.com>
2023-02-14 01:57:43 -0800

    add video

--- js/youtube/youtube-loadinfo.js
+++ js/youtube/youtube-loadinfo.js
@@ -1,6 +1,6 @@
 (function () {
     $(document).ready(function () {
-        const dataUpdateDate = 'Feb 9, 2023';
+        const dataUpdateDate = 'Feb 14, 2023';
         $(`div.last-updated span.right`).append(`Data last updated: ${dataUpdateDate}`);
     });
 }) ();
