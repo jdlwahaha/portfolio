e035817738ad1fc21a960b40ae04f6c360776260
JDL Wahaha <z29321136@hotmail.com>
2023-04-13 00:30:55 -0700

    fix mobile

---
+++ js/kittens/generate_collections.js
@@ -0,0 +1,53 @@
+(function () {
+    $(document).ready(function () {
+        $.getJSON('../../data/explodingkittens/editions.json', function (editions) {
+            const html = editions.map(edition => { 
+                const displayType = getType(edition.type);
+
+                const link = (edition.completed) 
+                    ? `
+                    <a href="${edition.filename}">
+                                ${edition.name} 
+                            </a><br>` : `${edition.name}<br>`; 
+
+                return `
+                    <tr>
+                        <td>
+                            ${link}
+                            <!--  <a href="${edition.filename}">
+                                ${edition.name} 
+                            </a><br> -->
+                            <!-- <span class="mobile">(${edition.release_year})</span><br> -->
+                            
+                            <span class="mobile">${displayType}</span><br>
+
+                            <div>
+                                <img src="../../data/explodingkittens/${edition.filename}.png" alt="${edition.filename}">
+                            </div>
+                        </td> 
+                        <td class="desktop">${displayType}</td>
+                        <td>$${edition.price}</td>
+                        <td>${edition.players}</td>   
+                        <td>${edition.cards}</td>                                                             
+                        <td class="desktop">${edition.release_year}</td>
+                    </tr>
+                `; 
+            }); 
+            $('#tableData').append(html);
+        });
+
+    })
+
+    function getType(type) { 
+        switch(type) { 
+            case 'full': 
+                return 'Full'; 
+            case 'full_no': 
+                return 'Full (Not compatible with other editions)'; 
+            case 'expansion': 
+                return 'Expansion'; 
+            default: 
+                return '';
+        }
+    }
+})();
