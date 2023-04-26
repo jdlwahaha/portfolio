957c3b9aac2cc42fc4caabea9ceb52385c5e27db
JDL Wahaha <z29321136@hotmail.com>
2023-04-25 18:05:49 -0700

    refactor kitten page to load better and update

--- js/kittens/get_details.js
+++ js/kittens/get_details.js
@@ -15,90 +15,19 @@
                     const fullDetail = { ...editionDetail, ...specificEditionDetail };
 
                     const isExpansion = (fullDetail.type === 'expansion'); 
-                    const expansionWarning = `
-                        <div class="well well-info" >
-                            This is an expansion pack. You will need one of the full game edition to play this game. 
-                        </div>`;
-
 
                     // get a list of card names 
                     const cardNames = Object.keys(fullDetail.cards);
-                    let displayCardNames = []; 
-                    let cardGroupImages = '';
-                    cardNames.map(name => {
-                        const images = fullDetail.cards[name].images;
-                        const description = fullDetail.cards[name].description;
-                        cardGroupImages += `<h4>${name} (${images.length})</h4><p>${description}</p>`;
-                        displayCardNames.push(name + ` (${images.length})`)
-                        
-                        images.map(i => { 
-                            cardGroupImages += `
-                            <span>
-                                <img src="../../data/explodingkittens/${fullDetail.filename}/IMG_${i}.jpeg" alt="${i}.jpeg">
-                            </span>
-                            `;
-                        });
-                        cardGroupImages += '<br><br>';
 
-                    });
+                    // load detail box first
+                    const detailBoxHtml = getDetailBoxHtml(cardNames, fullDetail, isExpansion);
+                    $('#detailBox').append(detailBoxHtml);
 
-                    $('#cards').append(cardGroupImages);
+                    // load card image list
+                    const cardGroupImageHtml = getCardGroupImageHtml(cardNames, fullDetail);
+                    $('#cards').append(cardGroupImageHtml);
 
-
-                    const contentList = fullDetail.contents.map(i => `<li>${i}</li>`).join('');
-                    const cardList = '<li>' + displayCardNames.join('</li><li>') + '</li>';
-
-                    const html = `
-                        <h1>${fullDetail.name}</h1>
-                            <div class="box-content">
-                                ${ isExpansion ? expansionWarning : '<p></p>'}
-            
-                                <div class="info-block">
-                                    <span>
-                                        <ul>
-                                            ${ isExpansion ? `<li><strong>Expansion Number:</strong> ${fullDetail.expansion_no}</li>` : '' }
-                                            <li>
-                                                <strong>Price:</strong> CDN$${fullDetail.price}    
-                                                ${getAmazonLink(fullDetail.amazon)}
-                                            </li>
-                                            <li>
-                                                <strong>Printed Year:</strong> ${fullDetail.printed_year}
-                                                <span class="triggerHover">
-                                                    &#9432;
-                                                    <span class="showOnHover">Printed year of my copy and the info on this page</span>
-                                                </span>
-                                                
-                                            </li>
-                                            <li>
-                                                <strong>Contents:</strong> 
-                                                <ul class="ul-nopadding">
-                                                    <li>${fullDetail.num_of_cards} Cards</li>
-                                                    ${contentList}
-                                                </ul>
-                                            </li>
-                                            <li><strong>Card Label:</strong> ${fullDetail.card_label} </li>
-                                            <li>
-                                                <strong>Cards:</strong>
-                                                <ul class="ul-nopadding">
-                                                    ${cardList}
-                                                </ul>
-                                            </li>
-                                        </ul>
-                                    </span>
-                                    <span>
-                                        <img src="../../data/explodingkittens/${fullDetail.filename}/all.jpeg" alt="all.jpeg">
-                                    </span>
-                                </div>
-                                
-                    
-                            </div>
-                        
-                        `;
-
-
-                    $('#detailBox').append(html);
-
-                    appendYouTube(fullDetail.youtubeId);
+                    appendYouTubeIframe(fullDetail.youtubeId);
                     
                 });
             } else {
@@ -109,7 +38,7 @@
 
     }); 
 
-    function appendYouTube(id) { 
+    function appendYouTubeIframe(id) { 
         if (id) { 
             $('#iframe').append(`
             <iframe 
@@ -123,4 +52,88 @@
         }
     }
 
+
+
+    function getDetailBoxHtml(cardNames, fullDetail, isExpansion) {
+
+        const expansionWarning = `
+            <div class="well well-info" >
+                This is an expansion pack. You will need one of the full game edition to play this game. 
+            </div>`;
+    
+        const displayCardNames = cardNames.map(name => `${name} (${fullDetail.cards[name].images.length})`);
+        const cardList = '<li>' + displayCardNames.join('</li><li>') + '</li>';
+        const contentList = fullDetail.contents.map(i => `<li>${i}</li>`).join('');
+    
+        const html = `
+                <h1>${fullDetail.name}</h1>
+                <div class="box-content">
+                    ${isExpansion ? expansionWarning : '<p></p>'}
+
+                    <div class="info-block">
+                        <span>
+                            <ul>
+                                ${isExpansion ? `<li><strong>Expansion Number:</strong> ${fullDetail.expansion_no}</li>` : ''}
+                                <li>
+                                    <strong>Price:</strong> CDN$${fullDetail.price}    
+                                    ${getAmazonLink(fullDetail.amazon)}
+                                </li>
+                                <li>
+                                    <strong>Printed Year:</strong> ${fullDetail.printed_year}
+                                    <span class="triggerHover">
+                                        &#9432;
+                                        <span class="showOnHover">Printed year of my copy and the info on this page</span>
+                                    </span>
+                                    
+                                </li>
+                                <li>
+                                    <strong>Contents:</strong> 
+                                    <ul class="ul-nopadding">
+                                        <li>${fullDetail.num_of_cards} Cards</li>
+                                        ${contentList}
+                                    </ul>
+                                </li>
+                                <li><strong>Card Label:</strong> ${fullDetail.card_label} </li>
+                                <li>
+                                    <strong>Cards:</strong>
+                                    <ul class="ul-nopadding">
+                                        ${cardList}
+                                    </ul>
+                                </li>
+                            </ul>
+                        </span>
+                        <span>
+                            <img src="../../data/explodingkittens/${fullDetail.filename}/all.jpeg" alt="all.jpeg">
+                        </span>
+                    </div>
+                </div>`;
+        return html;
+    }
+
+
+    function getCardGroupImageHtml(cardNames, fullDetail) {
+        let cardGroupImages = '';
+        cardNames.map(name => {
+            const images = fullDetail.cards[name].images;
+            const description = fullDetail.cards[name].description;
+            const descriptionHtml = Array.isArray(description)
+                ? `<p>${description.join('<p></p>')}</p>`
+                : `<p>${description}</p>`;
+            cardGroupImages += `<h4>${name} (${images.length})</h4>`;
+
+            images.map(i => {
+                cardGroupImages += `
+                                <span>
+                                    <img src="../../data/explodingkittens/${fullDetail.filename}/IMG_${i}.jpeg" alt="${i}.jpeg">
+                                </span>
+                                `;
+            });
+            cardGroupImages += '<br><br>';
+
+        });
+        return cardGroupImages;
+    }
+
+
 })();
+
