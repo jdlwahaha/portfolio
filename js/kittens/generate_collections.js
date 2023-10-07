8b8fc7e95ea154591c5d03dbc33a61cee546f55b
JDL Wahaha <z29321136@hotmail.com>
2023-10-07 08:55:09 -0700

    add kitten js files

---
+++ js/kittens/generate_collections.js
@@ -0,0 +1,63 @@
+(function () {
+    $(document).ready(function () {
+        $.getJSON('../../data/explodingkittens/editions.json', function (editions) {
+            const html = editions.map(edition => { 
+                const displayType = getType(edition.type);
+
+                
+                const image = (edition.filename === 'minions')
+                            ? `<img src="../../data/explodingkittens/${edition.filename}/package.gif" alt="${edition.filename}.gif">`
+                            : `<img src="../../data/explodingkittens/${edition.filename}/package.jpeg" alt="${edition.filename}.jpeg">`;
+                
+                const amazonLink = (edition.amazon) 
+                            ? getAmazonLink('', edition.amazon) 
+                            : '';
+                            
+                return `
+                    <tr>
+                        <td>
+                            <a href="detail?edition=${edition.filename}">
+                                ${edition.name} 
+                            </a><br>
+                            <div class="mobile-800">
+                                <div class="addMarginTop">${displayType}</div>
+                                <div class="addMarginTop mobile">
+                                    CDN$${edition.price} <br><br>
+                                    ${amazonLink}
+                                </div>
+                            </div>
+                            
+                        </td> 
+                        <td class=" center zoom">
+                            ${image} 
+                        </td>
+                        <td class="price desktop center">
+                            $${edition.price} <br>
+                            ${amazonLink}
+                        </td>
+                        <td class="desktop-800">${displayType}</td>
+                        <td class="center">${edition.players}</td>   
+                        <td class="center">${edition.num_of_cards}</td>                                                             
+                        <td class="desktop center">${edition.release_year}</td>
+                        <td class="desktop-700 center">${edition.acquisition_date}</td>
+                    </tr>
+                `; 
+            }); 
+            $('#tableData').append(html);
+        });
+
+    })
+
+    function getType(type) { 
+        switch(type) { 
+            case 'full': 
+                return 'Full Game'; 
+            case 'full_no': 
+                return 'Full Game (Not compatible with other editions or expansions)'; 
+            case 'expansion': 
+                return 'Expansion'; 
+            default: 
+                return '';
+        }
+    }
+})();
---
+++ js/kittens/get_details.js
@@ -0,0 +1,172 @@
+(function () {
+    $(document).ready(function () {
+        $.getJSON('../../data/explodingkittens/editions.json', function (editions) {
+
+            const editionName = $.urlParam('edition');
+            const available_editions_names = editions.map(edition => { 
+                return edition.filename;
+            }); 
+            
+            if (editionName && available_editions_names.indexOf(editionName) >= 0) {
+
+                $.getJSON(`../../data/explodingkittens/detail/${editionName}.json`, function (specificEditionDetail) {
+
+                    const editionDetail = editions.find(e => e.filename === specificEditionDetail.filename);
+                    const fullDetail = { ...editionDetail, ...specificEditionDetail };
+
+                    const isExpansion = (fullDetail.type === 'expansion'); 
+
+                    // get a list of card names 
+                    const cardNames = Object.keys(fullDetail.cards);
+
+                    // load detail box first
+                    const detailBoxHtml = getDetailBoxHtml(fullDetail, isExpansion);
+                    $('#detailBox').append(detailBoxHtml);
+
+                    // load card image list
+                    const cardGroupImageHtml = getCardGroupImageHtml(cardNames, fullDetail);
+                    $('#cards').append(cardGroupImageHtml);
+
+                    appendYouTubeIframe(fullDetail.youtubeId);
+                    appendPdf(fullDetail.filename);
+
+
+                    
+                });
+            } else {
+                window.location.href = window.location.origin + '/portfolio/html/explodingkittens';
+            }
+
+        })
+
+    }); 
+
+    function appendYouTubeIframe(id) { 
+        if (id) { 
+            $('#iframe').append(`
+            <iframe 
+                src="https://www.youtube.com/embed/${id}" 
+                title="YouTube video player" 
+                frameborder="0" 
+                allow="encrypted-media; gyroscope; picture-in-picture;" 
+                allowfullscreen
+            ></iframe>
+        `);
+        }
+    }
+
+    function appendPdf(id) { 
+        if (id) { 
+            $('#iframepdf').append(`
+            <iframe 
+                src="../../data/explodingkittens/${id}/rules.pdf" 
+            ></iframe>
+        `);
+        }
+    }
+
+
+    function getDetailBoxHtml(fullDetail, isExpansion) {
+
+        const expansionWarning = `
+            <div class="well well-info" >
+                This is an expansion pack. You will need one of the full game edition to play this game. 
+            </div>`;
+            
+        const amazonLink = (fullDetail.amazon) 
+            ? getAmazonLink('', fullDetail.amazon) 
+            : '';
+    
+        const contentList = fullDetail.contents.map(i => `<li>${i}</li>`).join('');
+    
+        const html = `
+                <h1>${fullDetail.name}</h1>
+                <div class="box-content">
+                    ${isExpansion ? expansionWarning : '<p></p>'}
+
+                    <div class="info-block">
+                        <span>
+                            <ul>
+                                ${isExpansion ? `<li><strong>Expansion Number:</strong> ${fullDetail.expansion_no}</li>` : ''}
+                                <li>
+                                    <strong>Price:</strong> CDN$${fullDetail.price}   
+                                    ${amazonLink} 
+                                </li>
+                                <li>
+                                    <strong>Printed Year:</strong> ${fullDetail.printed_year}
+                                    <span class="triggerHover">
+                                        &#9432;
+                                        <span class="showOnHover">Printed year of my copy and the info on this page</span>
+                                    </span>
+                                    
+                                </li>
+                                <li>
+                                    <strong>Contents:</strong> 
+                                    <ul class="ul-nopadding">
+                                        <li>${fullDetail.num_of_cards} Cards</li>
+                                        ${contentList}
+                                    </ul>
+                                </li>
+                                <li><strong>Card Label:</strong> ${fullDetail.card_label} </li>
+
+                                <li></li>
+
+                                <li>
+                                    <strong>Table of Contents:</strong>
+                                    <ul class="ul-nopadding">
+                                        <li>
+                                            <a href="#unbox">
+                                                Unbox with me
+                                            </a> 
+                                        </li>
+                                        <li>
+                                            <a href="#cardList">
+                                                Card list
+                                            </a> 
+                                        </li>
+                                        <li>
+                                            <a href="#instructions">
+                                                Instruction booklet
+                                            </a> 
+                                        </li>
+                                    </ul>
+                                </li>
+                            </ul>
+                        </span>
+                        <span>
+                            <img src="../../data/explodingkittens/${fullDetail.filename}/all.jpeg" alt="all.jpeg">
+                        </span>
+                    </div>
+                </div>`;
+        return html;
+    }
+
+   
+
+
+    function getCardGroupImageHtml(cardNames, fullDetail) {
+        let cardGroupImages = '';
+        cardNames.map(name => {
+            const images = fullDetail.cards[name].images;
+            // const description = fullDetail.cards[name].description;
+            // const descriptionHtml = Array.isArray(description)
+            //     ? `<p>${description.join('<p></p>')}</p>`
+            //     : `<p>${description}</p>`;
+            cardGroupImages += `<h4>${name} (${images.length})</h4>`;
+
+            images.map(i => {
+                cardGroupImages += `
+                                <span>
+                                    <img class="hide card" src="../../data/explodingkittens/${fullDetail.filename}/IMG_${i}.jpeg" alt="${i}.jpeg">
+                                </span>
+                                `;
+            });
+            cardGroupImages += '';
+
+        });
+        return cardGroupImages;
+    }
+
+
+})();
+
